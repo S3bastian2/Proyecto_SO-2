a3a3010a12844b6e6a862a7c950842ed6d7b0ef1
Sebastián Martínez <sebmartinezse@unal.edu.co>
2025-11-26 22:39:08 -0500

    Creación de TLB

---
+++ TLB.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Defino la función para calcular el numero de pagina.
+unsigned int calcular_num_pagina(unsigned int direccion) {
+    unsigned int num_pagina = direccion / 4096;
+    return num_pagina;
+}
+
+//Defino la función para calcular la dirección.
+unsigned int calcular_desplazamiento(unsigned int direccion) {
+    unsigned int despla = direccion % 4096;
+    return despla;
+}
+
+//Se debe definir la función que me va a convertir tanto al numero de pagina como al de desplazamiento en binario.
+void convertir_binario(unsigned int valor, int numero_bits) {
+    for (int j = numero_bits - 1; j >= 0; j--) {
+        unsigned int bits = (valor >> j) & 1;
+        printf("%u", bits);
+    }
+}
+
+int main() {
+    printf("Hola bienvenido a nuestro traductor de direcciones.\n");
+    printf("Ingresa alguna direccion de memoria:\n ");
+    unsigned int direccion; //Definimos la variable que va a guardar la dirrecion que le pasamos a las funciones.
+    scanf("%u", &direccion);
+    unsigned int numero_pagina = calcular_num_pagina(direccion);
+    printf("El numero de pagina sera: %u\n", numero_pagina);
+    //printf("Tamaño de la variable desplazamiento: %zu\n", sizeof(numero_pagina)); Al ser char unicamente ocupa un byte de almacenamiento.
+    unsigned int desplazamiento = calcular_desplazamiento(direccion);
+    printf("El desplazamiento sera: %u\n", desplazamiento);
+    //Convertimos el numero de pagina en binario. 
+    convertir_binario(numero_pagina, 20);
+    //Convertimos el numero del desplazamiento en binario.
+    convertir_binario(desplazamiento, 12);
+    unsigned int *tlb = (unsigned int *)malloc(300);
+    printf("\nInicio del TLB: %p y fin del TLB: %p\n", (void *)tlb, (void *)(tlb + 299));
+    free(tlb);
+}
